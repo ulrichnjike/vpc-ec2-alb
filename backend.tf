27f73948e3598d78cec1f74172a8a4960f876a71
ulrichnjike <ulrichnjike42.gmail.com>
2025-12-06 09:54:50 -0500

    no backendfile

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "week10-terraform"
-    key = "week10/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true 
-    dynamodb_table = "state-lock"
-  }
-}
